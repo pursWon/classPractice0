4df7711a5e3dc36b92904e11ec1dde9e639a53ea
Wongi <hpurs95@gmail.com>
2022-07-31 23:32:42 +0900

    Initial Commit

---
+++ classPractice0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  classPractice0
+//
+//  Created by MacBook Air on 2022/07/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ classPractice0/classPractice0App.swift
@@ -0,0 +1,17 @@
+//
+//  classPractice0App.swift
+//  classPractice0
+//
+//  Created by MacBook Air on 2022/07/31.
+//
+
+import SwiftUI
+
+@main
+struct classPractice0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
